b56c69846b84b7be5dd40fc58a1013279bb8ed89
EmreTocan <emretocan@gmail.com>
2025-01-28 16:18:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, request # type: ignore
+from flask import Flask, request 
 
 app = Flask(__name__)
 
-# Toplama endpoint
+
 @app.route('/toplama')
 def toplama():
     try:
-        # Query parametrelerini al ve virgül ile ayırarak liste yap
         parametreler = request.query_string.decode('utf-8').split(',')
         sayi1, sayi2 = map(int, parametreler)
         sonuc = sayi1 + sayi2
@@ -14,7 +13,6 @@ def toplama():
     except:
         return "Hata: Lütfen geçerli sayılar girin. Örnek: 2,2"
 
-# Çıkarma endpoint
 @app.route('/çıkarma') 
 def cikarma():
     try:
@@ -25,7 +23,6 @@ def cikarma():
     except:
         return "Hata: Lütfen geçerli sayılar girin. Örnek: 2,2"
 
-# Bölme endpoint
 @app.route('/bölme')
 def bolme():
     try:
